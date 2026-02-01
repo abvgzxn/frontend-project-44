7e3bdf50cf003c5aa8e7d643672f14bf6b5ec577
abvgzxn <tryonce499@gmail.com>
2026-02-01 21:10:17 +0300

    Add brain-gcd game: find greatest common divisor

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/abvgzxn/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/abvgzxn/frontend-project-44/actions)
 
+### SonarCloud:
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=abvgzxn_frontend-project-44&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=abvgzxn_frontend-project-44)
 
 ### Brain Games
@@ -9,3 +10,6 @@ https://asciinema.org/a/xPm3jnyfmR16iey7
 
 ### 2nd game: Calc
 https://asciinema.org/a/aay0V989gPTXXd3t
+
+### 3rd game: GCD
+https://asciinema.org/a/18FIXmGI5Qg9MjLm
---
+++ src/games/gcd.js
@@ -0,0 +1,33 @@
+import runGame from '../index.js';
+import crypto from 'crypto';
+
+// Функция для нахождения НОД (алгоритм Евклида)
+const findGCD = (a, b) => {
+  let num1 = a;
+  let num2 = b;
+
+  while (num2 !== 0) {
+    const temp = num2;
+    num2 = num1 % num2;
+    num1 = temp;
+  }
+
+  return num1;
+};
+
+const generateRound = () => {
+  const num1 = crypto.randomInt(1, 101);  // от 1 до 100
+  const num2 = crypto.randomInt(1, 101);  // от 1 до 100
+
+  const question = `${num1} ${num2}`;
+  const correctAnswer = String(findGCD(num1, num2));
+
+  return [question, correctAnswer];
+};
+
+const playGcd = () => {
+  const description = 'Find the greatest common divisor of given numbers.';
+  runGame(description, generateRound);
+};
+
+export default playGcd;
